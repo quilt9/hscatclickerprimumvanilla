ca8866a6cd3ae495ce3e7e4a3c19a746187bbcac
codestruct <hello@helenshiu.com>
2017-06-05 12:35:49 -0400

    Clean out funny bootstrap css from previous project.

--- _/src/js/script.js
+++ _/src/js/script.js
@@ -1,14 +1,41 @@
+/*
 document.addEventListener("DOMContentLoaded", function() { 
   // listen for clicks
-  var catainer1 = document.getElementById('catainer1');
-  var catainer2 = document.getElementById('catainer2');
+  var catainer1 = document.getElementById('catainer-1');
+  var catainer2 = document.getElementById('catainer-2');
+  var catImg1 = document.getElementById('catImg-1');
   catainer1.addEventListener('click', function() {
+    alert("You Click 1");
     // toggle kitten image with each click
-    if(catainer1.innerHTML=='<img class="img-responsive" alt="Responsive image" src="dist/images_src/kitten1.jpg">') {
-      catainer1.innerHTML='<img class="img-responsive" alt="Responsive image" src="dist/images_src/kitten2.jpg">';
+    if(catImg1.src='dist/images_src/kitten1.jpg') {
+      catImg1.src='/path/to/dist/images_src/kitten2.jpg';
     } else {
-      catainer1.innerHTML='<img class="img-responsive" alt="Responsive image" src="dist/images_src/kitten1.jpg">';
+      catImg1.src='/path/to/dist/images_src/kitten1.jpg';
     }
     
   })
+});
+*/
+
+// Shorthand for $( document ).ready()
+$(function() {
+  console.log( "ready!" );
+  $('#catImg-1').on({
+    'click': function() {
+      alert("You click 1");
+         var src = ($(this).attr('src') === 'dist/images_src/kitten1.jpg')
+            ? 'dist/images_src/kitten2.jpg'
+            : 'dist/images_src/kitten1.jpg';
+         $(this).attr('src', src);
+    }
+  });//catImg-1
+  $('#catImg-2').on({
+    'click': function() {
+      alert("You click 2");
+         var src = ($(this).attr('src') === 'dist/images_src/kitten3.jpg')
+            ? 'dist/images_src/kitten4.jpg'
+            : 'dist/images_src/kitten3.jpg';
+         $(this).attr('src', src);
+    }
+  });//catImg-1
 });
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,14 +1,22 @@
-/*! PizzaWebPageOptimization - v - 2017-06-05 */document.addEventListener("DOMContentLoaded", function() { 
-  // listen for clicks
-  var catainer1 = document.getElementById('catainer1');
-  var catainer2 = document.getElementById('catainer2');
-  catainer1.addEventListener('click', function() {
-    // toggle kitten image with each click
-    if(catainer1.innerHTML=='<img class="img-responsive" alt="Responsive image" src="dist/images_src/kitten1.jpg">') {
-      catainer1.innerHTML='<img class="img-responsive" alt="Responsive image" src="dist/images_src/kitten2.jpg">';
-    } else {
-      catainer1.innerHTML='<img class="img-responsive" alt="Responsive image" src="dist/images_src/kitten1.jpg">';
+/*! PizzaWebPageOptimization - v - 2017-06-05 */// Shorthand for $( document ).ready()
+$(function() {
+  console.log( "ready!" );
+  $('#catImg-1').on({
+    'click': function() {
+      alert("You click 1");
+         var src = ($(this).attr('src') === 'dist/images_src/kitten1.jpg')
+            ? 'dist/images_src/kitten2.jpg'
+            : 'dist/images_src/kitten1.jpg';
+         $(this).attr('src', src);
     }
-    
-  })
+  });//catImg-1
+  $('#catImg-2').on({
+    'click': function() {
+      alert("You click 2");
+         var src = ($(this).attr('src') === 'dist/images_src/kitten3.jpg')
+            ? 'dist/images_src/kitten4.jpg'
+            : 'dist/images_src/kitten3.jpg';
+         $(this).attr('src', src);
+    }
+  });//catImg-1
 });
