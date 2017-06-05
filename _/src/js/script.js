2ac44fcb2ab667b99174e62e4a17c46c9396b1e6
codestruct <hello@helenshiu.com>
2017-06-05 13:07:56 -0400

    Commit completed first changs.

--- _/src/js/script.js
+++ _/src/js/script.js
@@ -1,41 +1,30 @@
-/*
-document.addEventListener("DOMContentLoaded", function() { 
-  // listen for clicks
-  var catainer1 = document.getElementById('catainer-1');
-  var catainer2 = document.getElementById('catainer-2');
-  var catImg1 = document.getElementById('catImg-1');
-  catainer1.addEventListener('click', function() {
-    alert("You Click 1");
-    // toggle kitten image with each click
-    if(catImg1.src='dist/images_src/kitten1.jpg') {
-      catImg1.src='/path/to/dist/images_src/kitten2.jpg';
-    } else {
-      catImg1.src='/path/to/dist/images_src/kitten1.jpg';
-    }
-    
-  })
-});
-*/
-
 // Shorthand for $( document ).ready()
 $(function() {
   console.log( "ready!" );
   $('#catImg-1').on({
     'click': function() {
-      alert("You click 1");
-         var src = ($(this).attr('src') === 'dist/images_src/kitten1.jpg')
-            ? 'dist/images_src/kitten2.jpg'
-            : 'dist/images_src/kitten1.jpg';
-         $(this).attr('src', src);
-    }
+      //Change image
+      var src = ($(this).attr('src') === 'dist/images_src/kitten1.jpg')
+        ? 'dist/images_src/kitten2.jpg'
+        : 'dist/images_src/kitten1.jpg';
+      $(this).attr('src', src);
+      //Add one to counter
+      var counter = parseInt($('#catainer-1 span.counter').text());
+      ++counter;
+      $('#catainer-1 span.counter').text(counter);
+  }
   });//catImg-1
   $('#catImg-2').on({
     'click': function() {
-      alert("You click 2");
-         var src = ($(this).attr('src') === 'dist/images_src/kitten3.jpg')
-            ? 'dist/images_src/kitten4.jpg'
-            : 'dist/images_src/kitten3.jpg';
-         $(this).attr('src', src);
+      //Change image
+      var src = ($(this).attr('src') === 'dist/images_src/kitten3.jpg')
+        ? 'dist/images_src/kitten4.jpg'
+        : 'dist/images_src/kitten3.jpg';
+      $(this).attr('src', src);
+      //Add one to counter
+      var counter = parseInt($('#catainer-2 span.counter').text());
+      ++counter;
+      $('#catainer-2 span.counter').text(counter);
     }
-  });//catImg-1
+  });//catImg-2
 });
--- dist/js/main.js
+++ dist/js/main.js
@@ -3,20 +3,28 @@ $(function() {
   console.log( "ready!" );
   $('#catImg-1').on({
     'click': function() {
-      alert("You click 1");
-         var src = ($(this).attr('src') === 'dist/images_src/kitten1.jpg')
-            ? 'dist/images_src/kitten2.jpg'
-            : 'dist/images_src/kitten1.jpg';
-         $(this).attr('src', src);
-    }
+      //Change image
+      var src = ($(this).attr('src') === 'dist/images_src/kitten1.jpg')
+        ? 'dist/images_src/kitten2.jpg'
+        : 'dist/images_src/kitten1.jpg';
+      $(this).attr('src', src);
+      //Add one to counter
+      var counter = parseInt($('#catainer-1 span.counter').text());
+      ++counter;
+      $('#catainer-1 span.counter').text(counter);
+  }
   });//catImg-1
   $('#catImg-2').on({
     'click': function() {
-      alert("You click 2");
-         var src = ($(this).attr('src') === 'dist/images_src/kitten3.jpg')
-            ? 'dist/images_src/kitten4.jpg'
-            : 'dist/images_src/kitten3.jpg';
-         $(this).attr('src', src);
+      //Change image
+      var src = ($(this).attr('src') === 'dist/images_src/kitten3.jpg')
+        ? 'dist/images_src/kitten4.jpg'
+        : 'dist/images_src/kitten3.jpg';
+      $(this).attr('src', src);
+      //Add one to counter
+      var counter = parseInt($('#catainer-2 span.counter').text());
+      ++counter;
+      $('#catainer-2 span.counter').text(counter);
     }
-  });//catImg-1
+  });//catImg-2
 });
